d07ba08f8cae08a9ccc61b457c8e9462b548ddea
Sanjin888 <sanjinkurtalija@gmail.com>
2023-06-03 16:42:44 +0200

    added changes

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,57 +1,33 @@
-import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
-import G from '../assets/G.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
-import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
-import overlaybg from '../assets/overlaybg.png';
-import spoon from '../assets/spoon.svg';
-
-import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
-import gericht from '../assets/gericht.png';
-import hakulogo from '../assets/hakulogo.png';
-import logohaku from '../assets/logohaku.png';
-import hrana from '../assets/hrana.jpg';
-import pita from '../assets/pita.jpg';
-import riza from '../assets/riza.png';
-import restoran from '../assets/restoran.jpg';
-import vino from '../assets/vino.png';
-
-export default {
-  bg,
-  chef,
-  G,
-  gallery01,
-  gallery02,
-  gallery03,
-  gallery04,
-  knife,
-  logo,
-  menu,
-  overlaybg,
-  spoon,
+const images = {
+  bg: require('../assets/bg.png'),
+  chef: require('../assets/chef.png'),
+  gallery01: require('../assets/gallery01.png'),
+  gallery02: require('../assets/gallery02.png'),
+  gallery03: require('../assets/gallery03.png'),
+  gallery04: require('../assets/gallery04.png'),
+  knife: require('../assets/knife.png'),
+  logo: require('../assets/logo.png'),
+  menu: require('../assets/menu.png'),
+  overlaybg: require('../assets/overlaybg.png'),
+  spoon: require('../assets/spoon.png'),
  
-  findus,
-  laurels,
-  award01,
-  award02,
-  award03,
-  award05,
-  sign,
-  quote,
-  gericht,
+  findus: require('../assets/findus.png'),
+  laurels: require('../assets/laurels.png'),
+  award01: require('../assets/award01.png'),
+  award02: require('../assets/award02.png'),
+  award03: require('../assets/award03.png'),
+  award05: require('../assets/award05.png'),
+  sign: require('../assets/sign.png'),
+  quote: require('../assets/quote.png'),
+  gericht: require('../assets/gericht.png'),
   
-  logohaku,
-  hrana,pita,riza,restoran,vino
+  logohaku: require('../assets/logohaku.png'),
+  hrana: require('../assets/hrana.jpg'),
+  pita: require('../assets/pita.jpg'),
+  riza: require('../assets/riza.png'),
+  restoran: require('../assets/restoran.jpg'),
+  vino: require('../assets/vino.png'),
 };
+
+export default images;
+
