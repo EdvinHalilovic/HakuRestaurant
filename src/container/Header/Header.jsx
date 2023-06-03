922a11c555bf27eab0e2d98fe69323a2ed8f850c
EdvinHalilovic <edvin955@gmail.com>
2023-06-03 19:48:21 +0200

    fixed errors and bugs

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import {images} from '../../constants';
+import { images } from '../../constants';
 import './Header.css';
-import {SubHeading} from '../../components';
+import { SubHeading } from '../../components';
 
+const Header = () => {
+  const handleExploreMenu = () => {
+    // Navigate to the menu page
+    // You can use a routing library or any other navigation mechanism of your choice
+    window.location.href = '#menu';
+  };
 
-const Header = () => (
-  <div className='app__header app__wrapper section__padding' id='home'>
-    <div className='app__wrapper_info'>
-      <SubHeading title='Chase the new flavour'/>
-      <h1 className='app__header-h1'>The key to Fine Dining</h1>
-      <p className='p__opensans' style={{margin:'2rem 0'}}>One of the best pizza restaruant in Sarajevo.Enjoy in a good atmposphere and amazing food.We cant wait to see you.As our moto says.A recipe has no soul. You, as the cook, must bring soul to the recipe.</p>
-      <button type='button' className='custom__button'>Explore Menu</button>
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new flavour" />
+        <h1 className="app__header-h1">The key to Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: '2rem 0' }}>
+          One of the best pizza restaurants in Sarajevo. Enjoy in a good atmosphere and amazing food. We can't wait to see
+          you. As our motto says, "A recipe has no soul. You, as the cook, must bring soul to the recipe."
+        </p>
+        <button type="button" className="custom__button" onClick={handleExploreMenu}>
+          Explore Menu
+        </button>
+      </div>
+      <div className="app__wrapper_img"></div>
+      <img src={images.riza} alt="header img" style={{ width: '40%', height: 'auto' }} />
     </div>
-    <div className='app__wrapper_img'></div>
-    <img src={images.riza} alt="header img" style={{width: '40%', height: 'auto'}} />
-   
-  </div>
-);
+  );
+};
 
 export default Header;
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
-import {SubHeading} from "../../components";
-import {images, data} from "../../constants";
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
 import './Laurels.css';
 
-const AwardCard = ({award: {imgUrl, title, subtitle}}) => {
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
   <div className='app__laurels_awards-card'>
     <img src={imgUrl} alt='awards' />
     <div className="app__laurels_awards-card_content">
@@ -12,7 +11,7 @@ const AwardCard = ({award: {imgUrl, title, subtitle}}) => {
       <p className="p__opensans">{subtitle}</p>
     </div>
   </div>
-}
+);
 
 const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
@@ -21,12 +20,23 @@ const Laurels = () => (
       <h1 className="headtext__cormorant">Our Laurels</h1>
 
       <div className="app__laurels_awards">
-        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+        {data.awards.map((award, index) => (
+          <AwardCard
+            award={award}
+            key={award.title}
+            imgUrl={
+              index === 0 ? images.award01 :
+              index === 1 ? images.award02 :
+              index === 2 ? images.award03 :
+              images.award05
+            }
+          />
+        ))}
       </div>
     </div>
 
     <div className="app__wrapper_img">
-      <img src={images.pita} alt="laurels_img" /> 
+      <img src={images.pita} alt="laurels_img" />
     </div>
   </div>
 );
